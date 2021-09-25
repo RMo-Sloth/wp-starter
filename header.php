2492fcd3b9bd460de146331bd3593a0f0b4f6493
Rob Monhemius <robmonhemius@gmail.com>
2021-09-25 22:26:25 +0100

    wp_body_open and body_class

--- header.php
+++ header.php
@@ -5,8 +5,9 @@
   <meta charset=""<?php bloginfo('charset'); ?>>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body>
+<body <?php body_class(); ?>>
 <?php 
+wp_body_open();
 wp_nav_menu([ 
   'theme_location' => 'primary',
   'container' => 'nav',
