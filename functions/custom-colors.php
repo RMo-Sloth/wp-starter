5d4aaa01785da39633f768705bdac14760003829
Rob Monhemius <robmonhemius@gmail.com>
2021-10-07 19:37:29 +0200

    theme-color-1

--- functions/custom-colors.php
+++ functions/custom-colors.php
@@ -1,6 +1,21 @@
 <?php 
 
 function theme_customize_colors( $wp_customize ) {
+  //add color 1
+  $wp_customize->add_setting(
+    'theme_color_1',
+    [
+      'type' => 'theme_mod',
+      'default' => '#FFF',
+      'sanitize_callback' => 'sanitize_hex_color'
+    ]
+  );
+
+  $color_1_control = new WP_Customize_Color_Control( $wp_customize, 'theme_color_1', [
+    'label' => __( 'Color 1', 'wp-starter' ),
+    'section' => 'colors'
+  ]);
+  $wp_customize->add_control( $color_1_control );
 
   // add color 7
   $wp_customize->add_setting(
@@ -24,9 +39,11 @@ function theme_css_colors() {
   printf( 
     "<style>
       :root {
-        --color-7: %s
+        --color-1: %s;
+        --color-7: %s;
       }
     </style>",
+    get_theme_mod( 'theme_color_1', '#FFF' ),
     get_theme_mod( 'theme_color_7', '#333' )
   );
 }
