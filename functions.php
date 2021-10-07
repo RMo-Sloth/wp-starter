bdd4bbc6711be2a6da7b97d2fd68a8763b325b5f
Rob Monhemius <robmonhemius@gmail.com>
2021-10-07 17:44:48 +0200

    encapsulated footer and header css

--- functions.php
+++ functions.php
@@ -43,10 +43,26 @@ function theme_register_sidebars() {
 add_action( 'widgets_init', 'theme_register_sidebars' );
 
 function theme_register_main_stylesheet() {
-  // register the main stylesheet
+  // style.css
   wp_enqueue_style( 
     'general-stylesheet',
     get_stylesheet_uri(),
+    [],
+    wp_get_theme()->get('Version')
+  );
+
+  // css/footer.css
+  wp_enqueue_style( 
+    'footer-stylesheet',
+    get_stylesheet_directory_uri().'/css/footer.css',
+    [],
+    wp_get_theme()->get('Version')
+  );
+
+  // css/header.css
+  wp_enqueue_style( 
+    'header-stylesheet',
+    get_stylesheet_directory_uri().'/css/header.css',
     [],
     wp_get_theme()->get('Version')
   );
