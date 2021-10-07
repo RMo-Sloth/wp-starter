94d31cdb0394ccfc71480a6478af5528947b35df
Rob Monhemius <robmonhemius@gmail.com>
2021-10-07 20:26:14 +0200

    theme-color-4

--- functions/custom-colors.php
+++ functions/custom-colors.php
@@ -33,6 +33,23 @@ function theme_customize_colors( $wp_customize ) {
   ]);
   $wp_customize->add_control( $color_2_control );
 
+  // add color 4
+  $color_4 = 'f1f1f1';
+  $wp_customize->add_setting(
+    'theme_color_4',
+    [
+      'type' => 'theme_mod',
+      'default' => $color_4,
+      'sanitize_callback' => 'sanitize_hex_color'
+    ]
+  );
+
+  $color_4_control = new WP_Customize_Color_Control( $wp_customize, 'theme_color_4', [
+    'label' => __( 'Color 4', 'wp-tester' ),
+    'section' => 'colors'
+  ]);
+  $wp_customize->add_control( $color_4_control );
+
   // add color 7
   $wp_customize->add_setting(
     'theme_color_7',
@@ -57,11 +74,13 @@ function theme_css_colors() {
       :root {
         --color-1: %s;
         --color-3: %s;
+        --color-4: %s;
         --color-7: %s;
       }
     </style>",
     get_theme_mod( 'theme_color_1', '#FFF' ),
     get_theme_mod( 'theme_color_2', '#F9F9F9' ),
+    get_theme_mod( 'theme_color_4', '#f1f1f1' ),
     get_theme_mod( 'theme_color_7', '#333' )
   );
 }
