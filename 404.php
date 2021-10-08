a9939635bc54944c2f9321f7e5f4c0738be4cfff
Rob Monhemius <robmonhemius@gmail.com>
2021-10-08 18:40:34 +0200

    notify user of page-not-found

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php 
+get_header();
+
+print('<div class="page container">');
+    printf(
+        '<h1>%s</h1>',
+        __( '404: Page Not Found', 'wp-test' )
+    );
+    printf(
+        '<p>%s</p>',
+        __( "We couldn't find the webpage you are looking for. Maybe you can find what you are looking for by using the navigation", 'wp-test' )
+    );
+    // possibly override this form by adding searchform.php
+    // override the results page by adding search.php
+    get_search_form(); 
+    print('</div>');
+
+get_footer();
