be67995311e3c945f2c254fded9c7ef73850f797
Rob Monhemius <robmonhemius@gmail.com>
2021-10-11 03:43:20 +0200

    refactor: header.php

--- header.php
+++ header.php
@@ -1,17 +1,19 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-  <?php wp_head(); ?>
-  <meta charset=""<?php bloginfo('charset'); ?>>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body <?php body_class(); ?>>
 <?php 
-wp_body_open();
-wp_nav_menu([ 
-  'theme_location' => 'primary',
-  'container' => 'nav',
-  'container_class' => 'navbar',
-  'depth' => '2'
-]);
-?>
+print('<!DOCTYPE html>');
+printf('<html %s>', get_language_attributes() );
+printf('
+<head>
+  <meta charset="%s">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">',
+  get_bloginfo('charset')
+);
+  wp_head();
+print('</head>');
+  printf('<body %s>', 'class="' . esc_attr( implode( ' ', get_body_class() ) ) . '"' );
+    wp_body_open();
+    wp_nav_menu(
+      ['theme_location' => 'primary',
+      'container' => 'nav',
+      'container_class' => 'navbar',
+      'depth' => '2']
+    );
