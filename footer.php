02d49eca201bdd7342defaff8153397c86d7321c
Rob Monhemius <robmonhemius@gmail.com>
2021-10-11 03:57:50 +0200

    refactor: footer.php

--- footer.php
+++ footer.php
@@ -1,24 +1,18 @@
-<footer>
-  <div class="container">
-  <?php 
-  if( is_active_sidebar('footer-1') ):
-    dynamic_sidebar( 'footer-1' );
-  endif;
+<?php
+  print('<footer><div class="container">');
 
-  if( is_active_sidebar('footer-2') ):
-    dynamic_sidebar( 'footer-2' );
-  endif;
-  
-  if( is_active_sidebar('footer-3') ):
-    dynamic_sidebar( 'footer-3' );
-  endif;
-  
-  ?>
-  </div>
-  <div class="container">
-    <p class='disclaimer'>This theme has been provided by <a href='https://waardwebsites.nl'>waardwebsites.nl</a></p>
-  </div>
-</footer>
-<?php wp_footer(); ?>
-</body>
-</html>
+    if( is_active_sidebar('footer-1') ):
+      dynamic_sidebar( 'footer-1' );
+    endif;
+
+    if( is_active_sidebar('footer-2') ):
+      dynamic_sidebar( 'footer-2' );
+    endif;
+    
+    if( is_active_sidebar('footer-3') ):
+      dynamic_sidebar( 'footer-3' );
+    endif;
+    
+  print('</div></footer>');
+  wp_footer();
+print('</body></html>');
