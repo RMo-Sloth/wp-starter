d19ed44f108d4e02a3a95b2c718f8598c68d094a
Rob Monhemius <robmonhemius@gmail.com>
2021-10-11 21:02:16 +0200

    date

--- single.php
+++ single.php
@@ -10,11 +10,13 @@ if( have_posts() ):
             printf('
             <div class="meta">
                 <span>%s</span><br>
+                <span>%s</span><br>
                 <span>%s</span>
             </div>
             ',
-            __('By: ') . get_the_author_posts_link(),
-            __('Categories: ') . get_the_category_list( ', ' )
+            __('By: ', 'wp-tester') . get_the_author_posts_link(),
+            __('Categories: ', 'wp-tester') . get_the_category_list( ', ' ),
+            __('Date: ', 'wp-tester' ) . get_the_date('jS \of F Y')
             );
 
             the_content();
