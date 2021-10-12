afe14e6f4b48cd8cfe77dcd4d75aca60bf4c0035
Rob Monhemius <robmonhemius@gmail.com>
2021-10-12 19:40:22 +0200

    comments template

--- comments.php
+++ comments.php
@@ -2,4 +2,11 @@
 
 if( comments_open() === false ) return;
 
-comment_form();
+print('<aside style="padding-top: 1rem; border-top: 2px solid black; margin-top: 2rem">');
+    printf('<h1>%s</h1>', __('Comments', 'wp-starter') );
+    wp_list_comments();
+    print('<div class="comment-pagination" style="text-align: center;">');
+        paginate_comments_links();
+    print('</div>');
+    comment_form();
+print('</aside>');
