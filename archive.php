f3d811913dc111623d784a92f25ca80191a71297
Rob Monhemius <robmonhemius@gmail.com>
2021-10-12 21:31:09 +0200

    created archive page

---
+++ archive.php
@@ -0,0 +1,11 @@
+<?php 
+get_header();
+
+if( have_posts() ):
+    print( '<main class="container">' );
+        the_archive_title('<h1>', '</h1>');
+        get_template_part( 'partials/post-list' );
+    print( '</main>' );
+endif;
+
+get_footer();
