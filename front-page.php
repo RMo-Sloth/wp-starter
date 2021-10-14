6c2d23d667f35f04a560c4cdfe4513e0cede8c4c
Rob Monhemius <robmonhemius@gmail.com>
2021-10-14 02:45:42 +0200

    front-page-container

--- front-page.php
+++ front-page.php
@@ -5,14 +5,17 @@ if( is_active_sidebar( 'front-page-top' ) ):
   dynamic_sidebar( 'front-page-top' );
 endif;
 
-if( have_posts() ):
-  if( is_singular() ):
-    get_template_part( 'partials/page' );
+print( '<main class="container">' );
+  if( have_posts() ):
+    if( is_singular() ):
+      get_template_part( 'partials/page' );
+    else:
+      get_template_part( 'partials/post-list' );
+    endif;
   else:
-    get_template_part( 'partials/post-list' );
+    printf('<p><b>%s</b></p>', __( 'No posts could be found', 'wp-starter' ) );
   endif;
-else:
-  printf('<p><b>%s</b></p>', __( 'No posts could be found', 'wp-starter' ) );
-endif;
+print( '</main>' );
+
 
 get_footer();
