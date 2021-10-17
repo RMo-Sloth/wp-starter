ecf9b3876b863580d6347cfadb61d7159f9a0c23
Rob Monhemius <robmonhemius@gmail.com>
2021-10-17 23:46:34 +0200

    refactor: theme supports

--- functions/add-theme-support.php
+++ functions/add-theme-support.php
@@ -1,26 +1,17 @@
 <?php
 
-function theme_support_logo() {
-    add_theme_support( 'custom-logo', [
-      'height'               => 100,
-      'width'                => 200,
-      'flex-height'          => false,
-      'flex-width'           => false,
-    ] );
-}
-add_action('after_setup_theme', 'theme_support_logo');
+function theme_add_theme_support() {
 
-function theme_support_title_tag() {
-    add_theme_support( 'title-tag' );
-}
-add_action('after_setup_theme', 'theme_support_title_tag');  
-
-function theme_support_align_wide() {
   add_theme_support( 'align-wide' );
-}
-add_action('after_setup_theme', 'theme_support_align_wide');
 
-function theme_support_responsive_emmbeds() {
+  add_theme_support( 'custom-logo', [
+    'height'               => 100,
+    'width'                => 200,
+    'flex-height'          => false,
+    'flex-width'           => false,
+  ] );
   add_theme_support( 'responsive-embeds' );
+
+  add_theme_support( 'title-tag' );
 }
-add_action('after_setup_theme', 'theme_support_responsive_emmbeds');  
+add_action('after_setup_theme', 'theme_add_theme_support');
