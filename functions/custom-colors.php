066ae88c7eb7c57061dcf1d1374e5423faf647e0
Rob Monhemius <robmonhemius@gmail.com>
2021-10-19 07:33:07 +0200

    removed color 7

--- functions/custom-colors.php
+++ functions/custom-colors.php
@@ -32,19 +32,7 @@ function theme_customize_colors( $wp_customize ) {
     'section' => 'colors'
   ]);
   $wp_customize->add_control( $color_2_control );
-
-  // add color 7
-  $wp_customize->add_setting( 'theme_color_7', [
-    'type' => 'theme_mod',
-    'default' => '#333',
-    'sanitize_callback' => 'sanitize_hex_color'
-  ]);
-
-  $color_7_control = new WP_Customize_Color_Control( $wp_customize, 'theme_color_7', [
-    'label' => __( 'Color 7', 'wp-starter' ),
-    'section' => 'colors'
-  ]);
-  $wp_customize->add_control( $color_7_control );
+  
 }
 add_action( 'customize_register', 'theme_customize_colors' );
 
@@ -54,12 +42,10 @@ function theme_css_colors() {
       :root {
         --color-1: %s;
         --color-3: %s;
-        --color-7: %s;
       }
     </style>",
     get_theme_mod( 'theme_color_1', '#FFF' ),
     get_theme_mod( 'theme_color_2', '#F9F9F9' ),
-    get_theme_mod( 'theme_color_7', '#333' )
   );
 }
 add_action( 'wp_head', 'theme_css_colors');
