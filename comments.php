40eae1b24dacc2fc564eab52dd94d63294254fc5
Rob Monhemius <robmonhemius@gmail.com>
2021-10-20 20:03:27 +0200

    wrap comments in ul

--- comments.php
+++ comments.php
@@ -4,7 +4,9 @@ if( comments_open() === false ) return;
 
 print('<aside style="padding-top: 1rem; border-top: 2px solid black; margin-top: 2rem">');
     printf('<h1>%s</h1>', __('Comments', 'wp-starter') );
+    print('<ul class="comments-list">');
     wp_list_comments();
+    print( '</ul>' );
     print('<div class="comment-pagination" style="text-align: center;">');
         paginate_comments_links();
     print('</div>');
