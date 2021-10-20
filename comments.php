34c2d57442948040835604454a24d5e76dbe5048
Rob Monhemius <robmonhemius@gmail.com>
2021-10-21 01:23:27 +0200

    style comments

--- comments.php
+++ comments.php
@@ -5,7 +5,7 @@ if( comments_open() === false ) return;
 print('<aside style="" class="comments">');
     printf('<h1>%s</h1>', __('Comments', 'wp-starter') );
     print('<ul class="comments-list">');
-    wp_list_comments();
+    wp_list_comments([ 'avatar_size' => '64' ]);
     print( '</ul>' );
     print('<div class="comment-pagination" style="text-align: center;">');
         paginate_comments_links();
