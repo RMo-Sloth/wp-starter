ec1ba8b1cb6bc92ac5539a3f69940714c4b58c97
Rob Monhemius <robmonhemius@gmail.com>
2021-10-26 18:57:40 +0200

    bugfix double while loop

--- index.php
+++ index.php
@@ -2,16 +2,14 @@
 get_header();
 
 print( '<main class="container">' );
-  if( have_posts() ):
+  if( have_posts() && is_singular() ):
     while( have_posts() ): the_post();
-      if( is_singular() ):
-        the_title('<h1>', '</h1>');
-        the_content();
-        comments_template();
-      else:
-        get_template_part( 'partials/post-list' );
-      endif;
+      the_title('<h1>', '</h1>');
+      the_content();
+      comments_template();
     endwhile;
+  elseif( have_posts() ):
+    get_template_part( 'partials/post-list' );
   else:
     print('<p><b>No posts could be found</b></p>');
   endif;
